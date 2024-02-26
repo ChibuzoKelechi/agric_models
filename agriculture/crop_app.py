65334c588388d97d0c9ae1f5e96187a40d9d06a6
Chibuzo Kelechi <narutokelechi@gmail.com>
2024-02-26 14:16:21 +0100

    RF model, app

--- agriculture/crop_app.py
+++ agriculture/crop_app.py
@@ -1,13 +1,10 @@
 import streamlit as st
-import xgboost as xg
-from xgboost import Booster
 import numpy as np
-
-xgbooster = Booster()
+import joblib
 
 
 # Model loading and config
-crop_model = xg.XGBModel.load_model('model.json')
+crop_model = joblib.load('agriculture/model.pkl')
 
 crops = {
     0: 'rice',
@@ -29,27 +26,28 @@ crops = {
 
 # Streamlit app 
 st.set_page_config(
-    page_title='Agricultural suggestion app'
+    page_title='Crop recommender app'
 )
 
-st.header('Crop suggestion with ML')
+st.header('Crop recommendation with ML')
 st.markdown('''
-            This machine learning model/app was trained on **agricultural data**, in order to suggest crops to be cultivated,
-            based on environmental/soil conditions. The included crops are **rice, Soyabeans, banana beans, cowpeas, orange, maize, coffee, peas, groundnuts, mango, grapes, watermelon, apple,cotton**
+            This machine learning model/app was trained on **agricultural data**, in order to recommend crops to be cultivated,
+            with respect to the environmental/soil conditions. The included crops are **rice, Soyabeans, banana beans, cowpeas, orange, maize, coffee, peas, groundnuts, mango, grapes, watermelon, apple,cotton**
 ''')
 
 st.subheader('Model UI')
 
-nitrogen_level = st.slider(label='Select nitrogen value', min_value=50, max_value=200)
-phosphor_level = st.slider(label='Select Phosphorus value', min_value=50, max_value=200)
-potassium_level = st.slider(label='Select Potassium value', min_value=50, max_value=200)
-temperature =  st.slider(label='Select temperature level', min_value=15, max_value=30)
-humidity =  st.slider(label='Select Humidity level', min_value=15, max_value=30)
-ph =  st.slider(label='Select ph level', min_value=1, max_value=14)
-rainfall =  st.slider(label='Select rainfall level', min_value=50, max_value=300)
 
-data = [nitrogen_level, phosphor_level, potassium_level, temperature, humidity, ph, rainfall]
-instance = xg.DMatrix(data)
+N = st.slider(label=' nitrogen value', min_value=50, max_value=200)
+P = st.slider(label=' Phosphorus value', min_value=50, max_value=200)
+K = st.slider(label=' Potassium value', min_value=50, max_value=200)
+temperature =  st.slider(label=' temperature level', min_value=15, max_value=30)
+humidity =  st.slider(label=' Humidity level', min_value=15, max_value=30)
+ph =  st.slider(label=' ph level', min_value=1, max_value=14)
+rainfall =  st.slider(label=' rainfall level', min_value=50, max_value=300)
+
+instance = [[N, P, K, temperature, humidity, ph, rainfall]]
+
 
 def make_predictions(instance_data):
     predictions = crop_model.predict_proba(instance_data)
@@ -62,9 +60,9 @@ if st.button('Suggest crop'):
     predicted_class, certainty = make_predictions(instance)
     
     st.markdown(f'''
-            <div style="background-color: black; color: white; font-weight: bold; padding: 1rem; border-radius: 10px;">
+            <div style="background-color: black; color: white; font-weight: normal; padding: 1rem; border-radius: 10px;">
             <h4>Results</h4>
-             Suggested crop => <span style="font-weight: bold;">{crops[predicted_class]} </span> with <span style="font-weight: bold;">{certainty:.2f}% </span>certainty
+             Suggested crop => <br><span style="font-weight: bold;">{crops[predicted_class]} </span> <br> 
+             <span style="font-weight: bold;">Certainty: {certainty:.2f}% </span>
             </div>
     ''', unsafe_allow_html=True)
-    
